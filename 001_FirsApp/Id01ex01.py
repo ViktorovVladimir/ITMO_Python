f9aa65bea71017f09b1ae93ca1b240da900c7172
ViktorovVladimir <167399035+ViktorovVladimir@users.noreply.github.com>
2024-11-25 20:40:30 +0300

    - add first and second project
    - add project Hello World;
    - add project scale Image.

--- 001_FirsApp/Id01ex01.py
+++ 001_FirsApp/Id01ex01.py
@@ -1,1 +1,31 @@
-print('Hello, World')
+#My first program^-) on Python
+
+
+# print('Hello, World')
+
+##test variables
+# pi:float = 3.14    #clue
+
+# print(pi)
+
+# pi = pi + 1;
+
+# print( type(pi),  pi)
+
+# pi = 'This is pi'
+
+# print(type(pi), pi)
+
+import math
+
+
+SECONDS_PER_MINUTE = 60;
+
+
+print(type(math.pi), math.pi)
+
+math.pi += 1 
+
+print(type(math.pi), math.pi)
+
+print(SECONDS_PER_MINUTE * 23)
